6030a85b90913074bdd1b994242b49e59b7cf8d6
Никита Гордеев <nagordeev0106@yandex.ru>
2022-12-07 04:55:07 +0300

    try fix

--- RIP/asgi.py
+++ RIP/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for RIP project.
+ASGI config for tests_online_django project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RIP.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tests_online_django.settings')
 
 application = get_asgi_application()
